896dc4ec98238f337fb115b5cbb56e7373d811e7
Zan Markan <zan.markan@gmail.com>
2022-11-15 11:55:11 +0000

    fix scripts maybe

--- scripts/chapter_2.sh
+++ scripts/chapter_2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cp .circleci/chapters/config_2.yml .circleci/config.yml
+cp scripts/chapters/config_2.yml .circleci/config.yml
 rm -f .circleci/continue-config.yml
--- scripts/chapter_3.sh
+++ scripts/chapter_3.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cp .circleci/chapters/config_4.yml .circleci/config.yml
+cp scripts/chapters/config_4.yml .circleci/config.yml
 rm -f .circleci/continue-config.yml
 
